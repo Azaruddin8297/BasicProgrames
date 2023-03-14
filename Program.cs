72974a28cab2c65c0e7696951d5602af85da1bde
Azaruddin8297 <ajarpopo.com>
2023-03-14 20:50:52 +0530

    hello

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BasicPrograms
+{
+    public class Programs
+    {
+        public static void Main(string[] args)
+        {
+
+            //VowelsandConsonants.checkVowelsandConsonants();
+            //Swap.call();
+            //LargestNumber.Largest();
+            // QutotientandReaminder.FindQandR();
+            OddandEven.oddoreven();
+        }
+    }
+}
