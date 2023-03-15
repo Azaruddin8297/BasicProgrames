a9dc61c7c3e1757924e33eec0bda8df06c1ec318
Azaruddin8297 <ajarpopo.com>
2023-03-16 00:07:12 +0530

    Swap

---
+++ Swap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class Swap
+    {
+        public static void call()
+        {
+            int a = 10; int b = 20;
+
+            Console.WriteLine("Before Swap a =" + a + "b = " + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("Before After a =" + a + "b = " + b);
+        }
+    }
+}
