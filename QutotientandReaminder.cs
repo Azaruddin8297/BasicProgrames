947a0f3606db28d918b5c7e0d0aade8bcd322cb8
Azaruddin8297 <ajarpopo.com>
2023-03-16 00:03:25 +0530

    QandR

---
+++ QutotientandReaminder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class QutotientandReaminder
+    {
+        public static void FindQandR()
+        {
+            int divident = 61, divisor = 10;
+            int remainder = divident % divisor;
+            int Quotient = divident / divisor;
+            Console.WriteLine("the divident is {0} and divisor is {1} ",divident, divisor);
+            Console.WriteLine("The Qutient is {0} and Remainder is {1}", Quotient, remainder);
+        }
+    }
+}
