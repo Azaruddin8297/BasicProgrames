c5bbc3053f59c5b6648b98f4f0ec07fd4e7071e5
Azaruddin8297 <ajarpopo.com>
2023-03-16 00:12:42 +0530

    Factor

---
+++ Factor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class Factor
+    {
+        public static void call()
+        {
+            Console.WriteLine("Please enter a positive integer:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The factors of {0} are:", n);
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+
+        }
+    }
+}
